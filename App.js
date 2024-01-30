7400befca6f476866f171bac8b82adde2ba8b9d8
Glydewell Burdick <glydewellb@gmail.com>
2024-01-29 22:40:58 -0600

    create button components and option container

--- App.js
+++ App.js
@@ -6,12 +6,26 @@ import * as ImagePicker from 'expo-image-picker';
 
 import Button from './components/Button'; 
 import ImageViewer from './components/ImageViewer'; 
+import CircleButton from './components/CircleButton';
+import IconButton from './components/IconButton';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
   const [selectedImage, setSelectedImage] = useState(null);
   const [showAppOptions, setShowAppOptions] = useState(false);
+
+  const onReset = () => {
+    setShowAppOptions(false);
+  };
+
+  const onAddSticker = () => {
+    // we will implement this later
+  };
+
+  const onSaveImageAsync = async () => {
+    // we will implement this later
+  };
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -36,8 +50,14 @@ export default function App() {
         />
       </View>
     {showAppOptions ? (
-      <View />
-    ) : (
+        <View style={styles.optionsContainer}>
+        <View style={styles.optionsRow}>
+          <IconButton icon="refresh" label="Reset" onPress={onReset} />
+          <CircleButton onPress={onAddSticker} />
+          <IconButton icon="save-alt" label="Save" onPress={onSaveImageAsync} />
+        </View>
+      </View>
+  ) : (
       <View style={styles.footerContainer}>
         <Button theme="primary" label="Choose a Photo" onPress={pickImageAsync} />
         <Button label="Use this Photo" onPress={() => setShowAppOptions(true)} />
@@ -62,5 +82,13 @@ const styles = StyleSheet.create({
   footerContainer: {
     flex: 1 / 3,
     alignItems: 'center',
+  },
+  optionsContainer: {
+    position: 'absolute',
+    bottom: 80,
+  },
+  optionsRow: {
+    alignItems: 'center',
+    flexDirection: 'row',
   },
 });
